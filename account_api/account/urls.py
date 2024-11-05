4b6a5728674ba9e35ce1ece757f0ef722dcd357b
Tahiru Swallah <swallaht3@gmail.com>
2024-11-05 06:48:22 +0000

    edit

--- account_api/account/urls.py
+++ account_api/account/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import LoginView, RegistrationView, MyPublicView, Api_Root, PasswordChangeView
 
 urlpatterns = [
-    path('api-root/', Api_Root.as_view(), name='api_root'),
+    path('', Api_Root.as_view(), name='api_root'),
     path('login/', LoginView.as_view(), name='login_api'),
     path('sgnup/', RegistrationView.as_view(), name='signup_api'),
     path('my_public_api/',MyPublicView.as_view(), name='mypublicapi'),
