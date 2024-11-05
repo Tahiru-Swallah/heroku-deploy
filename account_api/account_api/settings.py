829b22043c2796acff96db28f5670bd33531b036
Tahiru Swallah <swallaht3@gmail.com>
2024-11-05 06:58:09 +0000

    added changes

--- account_api/account_api/settings.py
+++ account_api/account_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n#9n)754lwgppzs&i44y5+#cl)mj*!wqohfsk!zum!(!q10=)m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', 'localhost']
 
 
 # Application definition
